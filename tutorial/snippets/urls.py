43263596804e8b57f19ded4b658e28d84c8e788e
Etienne Schalk <etienne.schalk@grenoble-inp.org>
2020-11-14 12:07:35 +0100

    /tutorial/2-requests-and-response

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+# Allow to append to URL a suffix to choose the display format we want:
+# - .json: force JSON
+# - .api: force Browsable API (HTML)
+# Or add HTTP accept headers:
+# - Accept:application/json  # Request JSON
+# - Accept:text/html         # Request HTML
+urlpatterns = format_suffix_patterns(urlpatterns)
