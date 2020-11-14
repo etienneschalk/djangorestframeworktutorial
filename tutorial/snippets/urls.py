62acd37bbec893cee41e3693e8fbde1c33656ad9
Etienne Schalk <etienne.schalk@grenoble-inp.org>
2020-11-14 13:36:02 +0100

    /tutorial/5-relationships-and-hyperlinked-apis/

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,11 +2,27 @@ from django.urls import path
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+# ! Not: the following urlpatterns is not wrapped in format_suffix_patterns
+# (in contrary to the 5th tutorial page) because it has already been added
+# added before (in 2nd tutorial page)
+# API endpoints
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),
+    path('snippets/',
+         views.SnippetList.as_view(),
+         name='snippet-list'),
+    path('snippets/<int:pk>/',
+         views.SnippetDetail.as_view(),
+         name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+         views.SnippetHighlight.as_view(),
+         name='snippet-highlight'),
+    path('users/',
+         views.UserList.as_view(),
+         name='user-list'),
+    path('users/<int:pk>/',
+         views.UserDetail.as_view(),
+         name='user-detail')
 ]
 
 # Allow to append to URL a suffix to choose the display format we want:
