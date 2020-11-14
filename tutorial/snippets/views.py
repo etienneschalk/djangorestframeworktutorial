8eaa26dbe02e674e4597b35c8496a5bbc415aca9
Etienne Schalk <etienne.schalk@grenoble-inp.org>
2020-11-14 12:58:24 +0100

    /tutorial/4-authentication-and-permissions/ Read only for all, create for authenticated and update or delete for owner

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from snippets.models import Snippet
 from django.contrib.auth.models import User
 
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 
@@ -10,6 +12,9 @@ from snippets.serializers import UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+    ]
 
     # Override the default way of crating a new instance, to include the owner in the Snippet
     def perform_create(self, serializer):
@@ -19,6 +24,10 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    ]
 
 # List Read Only
 class UserList(generics.ListAPIView):
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -22,14 +22,14 @@ from django.urls import path
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
