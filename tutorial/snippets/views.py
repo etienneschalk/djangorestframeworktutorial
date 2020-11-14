3cc54ccc2ea24d8f2975d6ac0d4563f2b63b1d69
Etienne Schalk <etienne.schalk@grenoble-inp.org>
2020-11-14 12:35:31 +0100

    /tutorial/4-authentication-and-permissions/ Code snippets are always associated with a creator.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,13 +1,32 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+
+from snippets.models import Snippet
+from django.contrib.auth.models import User
+
+from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    # Override the default way of crating a new instance, to include the owner in the Snippet
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+# List Read Only
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Detail Read Only
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
